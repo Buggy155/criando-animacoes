ee8390a4c971d4e4508be4626b63627ae9ed8855
ana julia <144802566+Buggy155@users.noreply.github.com>
2024-03-15 07:44:07 -0300

    salvar

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let cor;
+let posicaoHorizontal;//x
+let posicaoVertical;//y
+
+function setup() {
+  
+  createCanvas(700,500);
+  background("pink");
+  cor= color ("black")
+  posicaoHorizontal = 200;
+  posicaoVertical = 200;
+}
+
+function draw(){
+  
+  fill(cor);
+  circle(posicaoHorizontal, posicaoVertical, 80);
+  
+  if(mouseX < posicaoHorizontal){
+    posicaoHorizontal--;
+  }
+  
+   if(mouseX > posicaoHorizontal){
+    posicaoHorizontal++
+   }
+  
+  if(mouseY < posicaoVertical){
+    posicaoVertical--;
+  }
+  
+  if(mouseY > posicaoVertical){
+    posicaoVertical++;
+  }
+  
+  if(mouseIsPressed){
+    cor = color ("cyan");
+  }
+  
+  
+}
